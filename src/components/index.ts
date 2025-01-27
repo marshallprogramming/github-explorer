e6ba75369f4ac2a030d8d52f13ef4183ca73cda3
marshallprogramming <42378943+marshallprogramming@users.noreply.github.com>
2025-01-27 23:56:09 +0000

    feat: Add back to home button when on individual user page

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export * from "./ui";
 export * from "./layout";
 export * from "./user";
+export * from "./icons";
--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,11 +1,28 @@
 import { FC } from "react";
-import { ThemeToggle } from "@/components";
+import { Home, ThemeToggle } from "@/components";
+import { Link, useLocation } from "react-router-dom";
+
+import clsx from "clsx";
 
 const Header: FC = () => {
+  const location = useLocation();
+  const isHome = location.pathname === "/";
+
   return (
     <header className="border-b border-gray-200 dark:border-gray-800">
-      <div className="container mx-auto px-4 py-4 flex justify-end items-center">
-        <ThemeToggle />
+      <div className="container mx-auto px-4 py-4 flex justify-between items-center">
+        {!isHome && (
+          <Link
+            className="cursor-pointer hover:opacity-100 opacity-80 transition-opacity ease-in-out"
+            to={"/"}
+            aria-label="Home"
+          >
+            <Home className="w-5 h-5" />
+          </Link>
+        )}
+        <div className={clsx(isHome && "ml-auto")}>
+          <ThemeToggle />
+        </div>
       </div>
     </header>
   );
